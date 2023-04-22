919e742ea88e86750eaa848a775b89f3b78bc53d
Minh Tran <tdaimike@gmail.com>
2023-04-22 12:58:51 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name :'Steve',
+        email : 'steve@steve.com',
+    }
+    const congiObj = {
+        method : "POST", 
+        headers : {
+        "Content-Type" : "application/json",
+        "accept" : "application/json",
+        },
+        body : JSON.stringify(formData),
+    }
+    return fetch ('http://localhost:3000/users', congiObj)
+    .then ((resp => resp.json()))
+    .then((obj) => {
+        const id = obj.id;
+        document.body.innerHTML =+ id;
+    })
+
+    .catch ((error) => document.body.innerHTML += error.message)
+}
+submitData();
